de7ec5a1a781d6b929353b9f967494f0204baf18
sadafhasan <hasansadaf1995@gmail.com>
2018-06-30 12:25:00 +0530

    Chages in login page ui.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
 
-import { SignUpLink } from './SignUp';
-import { auth } from '../firebase';
+import {SignUpLink} from './SignUp';
+import {auth} from '../firebase';
 import * as routes from '../constants/routes';
 
-const SignInPage = ({ history }) =>
-    <div>
-        <h1>SignIn</h1>
-        <SignInForm history={history} />
-        <SignUpLink />
+const SignInPage = ({history}) =>
+    <div className="col-md-6">
+        <h1 className="page-header">LogIn</h1>
+        <SignInForm history={history}/>
+        {/*<SignUpLink />*/}
     </div>
 
 const byPropKey = (propertyName, value) => () => ({
@@ -26,7 +26,7 @@ class SignInForm extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { ...INITIAL_STATE };
+        this.state = {...INITIAL_STATE};
     }
 
     onSubmit = (event) => {
@@ -41,8 +41,8 @@ class SignInForm extends Component {
 
         auth.doSignInWithEmailAndPassword(email, password)
             .then((result) => {
-                this.setState(() => ({ ...INITIAL_STATE }));
-                console.log("Result::::"+result.user.email);
+                this.setState(() => ({...INITIAL_STATE}));
+                console.log("Result::::" + result.user.email);
                 history.push(routes.HOME);
             })
             .catch(error => {
@@ -64,25 +64,27 @@ class SignInForm extends Component {
             email === '';
 
         return (
-            <form onSubmit={this.onSubmit}>
-                <input
-                    value={email}
-                    onChange={event => this.setState(byPropKey('email', event.target.value))}
-                    type="text"
-                    placeholder="Email Address"
-                />
-                <input
-                    value={password}
-                    onChange={event => this.setState(byPropKey('password', event.target.value))}
-                    type="password"
-                    placeholder="Password"
-                />
-                <button disabled={isInvalid} type="submit">
-                    Sign In
-                </button>
+            <div class="panel panel-default" className="col-md-6">
+                <form onSubmit={this.onSubmit}>
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                        </h4>
+                    </div>
+                    <div class="panel-body">
+                        <form className='form-group' onSubmit={this.onSubmit}>
+                            <input value={email} onChange={event => this.setState(byPropKey('email', event.target.value))}
+                                   name={'username'} type='email' placeholder='Email'
+                                   className='form-control' required/>
+                            <input value={password} onChange={event => this.setState(byPropKey('password', event.target.value))}
+                                   name={'password'} type='password' placeholder='Password'
+                                   className='form-control' required/>
+                            <input type='submit' value='Login' className='btn btn-primary btn-block'/>
+                        </form>
+                    </div>
+                    { error && <p>{error.message}</p> }
+                </form>
+            </div>
 
-                { error && <p>{error.message}</p> }
-            </form>
         );
     }
 }
